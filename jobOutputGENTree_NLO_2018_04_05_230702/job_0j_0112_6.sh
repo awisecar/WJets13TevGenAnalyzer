d8473b2b473efbb941fb7a94bfe4106a37b28abf
Andrew Wisecarver <andrew.lloyd.wisecarver@cern.ch>
2018-04-17 18:15:25 +0200

    small fix

--- jobOutputGENTree_NLO_2018_04_05_230702/job_0j_0112_6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-cd /afs/cern.ch/user/a/awisecar/WJetsGenMaker16/CMSSW_8_0_20/src/WJets13TeV/WJets13TevGenAnalyzer 
-
-cd /afs/cern.ch/user/a/awisecar/WJetsGenMaker16/CMSSW_8_0_20/src 
-export SCRAM_ARCH=slc6_amd64_gcc530 
-eval `scramv1 runtime -sh` 
-cmsRun /afs/cern.ch/user/a/awisecar/WJetsGenMaker16/CMSSW_8_0_20/src/WJets13TeV/WJets13TevGenAnalyzer/wjetsGENTreeMaker_batch_cfg.py JetNum=0 VariationNum=112 OutFileNum=6 2>&1 
-
